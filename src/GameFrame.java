9e533250e4f3c1851369ec647c6f2cdc8c04e6c6
RonkZeDonk <20400064+RonkZeDonk@users.noreply.github.com>
2022-01-12 13:26:40 -0500

    add back the button

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import javax.swing.*;
@@ -6,6 +8,7 @@ import javax.swing.*;
 public class GameFrame extends JFrame implements KeyListener {
 	public JPanel cardsPanel;
 	public JScrollPane cardsScrollPane;
+  private boolean cardsPanelOpen = false;
 
 	public JPanel tablePanel;
 
@@ -28,30 +31,56 @@ public class GameFrame extends JFrame implements KeyListener {
 	private void createCardsSection() {
 		cardsPanel = new JPanel();
 		cardsPanel.setBackground(new Color(0x654B2A));
-		
+
 		cardsScrollPane = new JScrollPane(cardsPanel);
-		cardsScrollPane.setBounds(0, 450, 985, 330);
+    cardsScrollPane.setBounds(0, 500, 985, 330);
 		cardsScrollPane.getHorizontalScrollBar().setUnitIncrement(24);
 		add(cardsScrollPane);
+
+    // △▽  ▲▼
+		JButton expandCardsPanel = new JButton("▲");
+		expandCardsPanel.setBounds(470, 475, 60, 25);
+		expandCardsPanel.setFocusable(false);
+		expandCardsPanel.setBackground(Color.white);
+		expandCardsPanel.setBorder(BorderFactory.createLineBorder(Color.lightGray, 1));
+		expandCardsPanel.setToolTipText("Expand/Shrink Cards Drawer");
+		expandCardsPanel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (!cardsPanelOpen) {
+					// Open drawer
+					cardsScrollPane.setLocation(0, 232);
+					expandCardsPanel.setLocation(470, 207);
+					expandCardsPanel.setText("▼");
+				} else {
+					// Close drawer
+					cardsScrollPane.setLocation(0, 500);
+					expandCardsPanel.setLocation(470, 475);
+					expandCardsPanel.setText("▲");
+				}
+				cardsPanelOpen = !cardsPanelOpen;
+			}
+		});
+		expandCardsPanel.setBounds(470, 475, 60, 25);
+		add(expandCardsPanel);
 	}
-	
+
 	public void refreshCardsFrame() {
 		cardsScrollPane.invalidate();
 		cardsScrollPane.validate();
 		cardsScrollPane.repaint();
 	}
-	
+
 	public void displayHandOnFrame(Hand hand) {
 		this.cardsPanel.removeAll();
-		
+
 		hand.cards.forEach(n -> this.cardsPanel.add(n.cardPanel));
 	}
-	
+
 	public void removeCardFromFrame(Card card) {
 		this.cardsPanel.remove(card.cardPanel);
 		refreshCardsFrame();
 	}
-	
+
 	private void createTableSection() {
 		tablePanel = new JPanel();
 		tablePanel.setLayout(null);
